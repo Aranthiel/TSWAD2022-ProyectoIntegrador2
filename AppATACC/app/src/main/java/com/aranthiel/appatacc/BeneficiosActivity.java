8cd30570d4d2179a3bc7403fdee5990a29822c95
Aranthiël (Natalia Moroni) <nmoronidalmasso@gmail.com>
2023-10-11 03:37:47 -0300

    FIX: se corrige el icono de la aplicacion y la vista de los beneficios

--- AppATACC/app/src/main/java/com/aranthiel/appatacc/BeneficiosActivity.java
+++ AppATACC/app/src/main/java/com/aranthiel/appatacc/BeneficiosActivity.java
@@ -5,15 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class BeneficiosActivity extends AppCompatActivity {
     TextView btnVolver;
+    ScrollView scrollView;
+
+    LinearLayout cardUno;
+    LinearLayout cardOcho;
+    LinearLayout cardDos;
+    LinearLayout cardTres;
+    LinearLayout cardCuatro;
+    LinearLayout cardCinco;
+    LinearLayout cardSeis;
+    LinearLayout cardSiete;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beneficios);
         btnVolver=findViewById(R.id.BeneficiosPageBtnVolver);
+        scrollView=findViewById(R.id.BeneficiosPageScollView);
+        cardUno=findViewById(R.id.BeneficiosPageCard1);
+        cardOcho=findViewById(R.id.BeneficiosPageCard8);
+        cardDos=findViewById(R.id.BeneficiosPageCard2);
+        cardTres=findViewById(R.id.BeneficiosPageCard3);
+        cardCuatro=findViewById(R.id.BeneficiosPageCard4);
+        cardCinco=findViewById(R.id.BeneficiosPageCard5);
+        cardSeis=findViewById(R.id.BeneficiosPageCard6);
+        cardSiete=findViewById(R.id.BeneficiosPageCard7);
 
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
--- AppATACC/app/src/main/java/com/aranthiel/appatacc/CalculadoraActivity.java
+++ AppATACC/app/src/main/java/com/aranthiel/appatacc/CalculadoraActivity.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.net.Uri;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class CalculadoraActivity extends AppCompatActivity {
 
     TextView btnVolver;
+    LottieAnimationView calcLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,26 @@ public class CalculadoraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calculadora);
 
         btnVolver=findViewById(R.id.CalculadoraPageBtnVolver);
+        calcLink=findViewById(R.id.CalculadoraPageCalcLink);
 
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(CalculadoraActivity.this, BienvenideActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        calcLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // URL que deseas abrir en el navegador
+                String url = "https://aranthiel.github.io/CalculadorATACC/";
+
+                // Crea un Intent para abrir el navegador web
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+                // Inicia el navegador web
                 startActivity(intent);
             }
         });
